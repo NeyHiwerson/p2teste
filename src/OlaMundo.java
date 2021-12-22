504c1deb6aeb1120cd8b243a83e4f8595c628775
Ney Hiwerson Missias Ribeiro <hiwerson@gmail.com>
2021-12-22 11:05:59 -0300

    first commit
    criando no intellij sem main

---
+++ src/OlaMundo.java
@@ -0,0 +1,12 @@
+public class OlaMundo {
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo123456!!!");
+        // na media ta levando 8 segundos para compilar o codigo
+        // na media ta levando 11 segundos para compilar o codigo com 6gb
+        // na media ta levando 16 segundos para compilar o codigo com 8gb
+        // na media ta levando 11 segundos para compilar o codigo com 8gb
+
+
+
+    }
+}
